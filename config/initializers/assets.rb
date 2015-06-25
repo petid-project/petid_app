255b4e3269e8089f90c4f5282aba540cba90972e
andreasbloomquist <andreasbloomquist@gmail.com>
2015-06-24 23:08:38 -0700

    major styles

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,7 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( app.js )
+Rails.application.config.assets.precompile += %w( welcome.css )
+Rails.application.config.assets.precompile += %w( reports.css )
+Rails.application.config.assets.precompile += %w( users.css )
+Rails.application.config.assets.precompile += %w( pets.css )
