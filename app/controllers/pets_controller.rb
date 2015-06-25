8791dc5141e32e607782a1962223d35e2c46bbe0
David Deuber <ddeuber@fastmail.fm>
2015-06-24 19:31:52 -0700

    added delete to profile, and updated lost dog show page

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -56,9 +56,10 @@ class PetsController < ApplicationController
   # DELETE /pets/1
   # DELETE /pets/1.json
   def destroy
+    @pet.reports.destroy_all unless @pet.reports.empty?
     @pet.destroy
     respond_to do |format|
-      format.html { redirect_to pets_url, notice: 'Pet was successfully destroyed.' }
+      format.html { redirect_to user_url(@pet.user), notice: 'Pet was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
