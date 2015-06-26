e9bd610584443952d4a23b04ec78479c0cdfb3e2
cachrisman <charlie.chrisman@gmail.com>
2015-06-26 10:04:45 -0700

    turbolinks fix

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,3 +1,10 @@
+$(document).on('page:load', function () {
+    $('#table_id').DataTable();
+    $( "th" ).removeClass( "sorting" );
+    $( "th" ).removeClass( "sorting_asc" );
+    $('input').addClass('form-control');
+} );
+
 $(document).ready( function () {
     $('#table_id').DataTable();
     $( "th" ).removeClass( "sorting" );
